30431fe7cdeb07c86b0589e443ec675e99ddd543
Daniel Murillo <danielmgiant@gmail.com>
2023-06-19 20:32:27 -0600

    Uploaded new app version

--- app/app.py
+++ app/app.py
@@ -4,12 +4,16 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask_cors import CORS
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import VotingClassifier, RandomForestClassifier
+from sklearn.metrics import precision_score, recall_score, confusion_matrix, roc_curve, precision_recall_curve, accuracy_score, classification_report
+import pickle5
 
 # Imported Flask, jsonify, request, and render_template to create and run the API:
 from flask import Flask, jsonify, request, render_template
 
 # Set up the database:
-engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost/NOMBRE_DE_LA_BASE_DE_DATOS")
+engine = create_engine("postgresql+psycopg2://postgres:pleycode@localhost/breast_cancer")
 
 # Reflected the existing database into a new model:
 Base = automap_base()
@@ -20,17 +24,34 @@ Base.prepare(autoload_with=engine)
 Base.classes.keys()
 
 # Saved the references to the main table:
-Nonvoter = Base.classes.responses
+tumor_measures = Base.classes.tumor_measures
 
 # Created an app making sure to pass __name__:
 app = Flask(__name__)
 CORS(app)
 
-# Flask routes (there are eight):
+# Flask routes:
 
-# 1. Main route:
+# 1. Home route:
 # Defined the end point:
 @app.route("/")
+def home():
+    print("Server received request for 'Home' page...")
+    return ("Hola! Como estas?")
+
+
+
+# Main route:
+
+# importar variables (inputs del dashboard)
+# darles el formato adecuado a las variables
+
+# importar modelo RF con pickle (scaler)
+
+# hacer predicciones
+
+# mandar la clasificacion de la prediccion
+
 
 
 # To run the app:
